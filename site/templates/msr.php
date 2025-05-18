df482245157fc520e5477c777899804db692bd37
Tomoyasu <rodenkirchenmax@gmail.com>
2025-05-19 00:03:28 +0200

    Authors

--- site/templates/msr.php
+++ site/templates/msr.php
@@ -16,14 +16,16 @@
 </head>
 
 <body>
-    <h2 id="logo">The Creative Coding Community Garden</h2>
+    <h2 id="logo">A Creative Coding Community Garden</h2>
 
 
     <!-- <h1 class="bgPanel"><?= $page->title() ?></h1> -->
     <!-- <h2 class="bgPanel"><?= $page->subtitle() ?></h2> -->
     <!-- <img src=<?= $page->images() ?> class="bgPanel"> -->
     <p><?= kirbytext($page->description()) ?></p>
+    <br>
 
+    <!-- LINKS -->
     <?php 
         if($page->link()->isNotEmpty()): 
             $linkedPages = $page->link()->toPages();
@@ -35,6 +37,14 @@
             endif;
         endif; 
     ?>
+
+    <br><br>
+    <!-- Last Edited -->
+    <p>Edited by <br>
+        <?php foreach ($page->author()->toUsers() as $user): ?>
+        <?= $user->name() ?> <br>
+        <?php endforeach; ?>
+    </p>
 
 
 </body>
